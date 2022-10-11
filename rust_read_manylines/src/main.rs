2443f0e4911f041a4c74082c641fa56ed9d35b76
DaisukeSugiyama-MT <sugiyama@modern-tech.co.jp>
2022-10-12 02:02:16 +0900

    行数が多いファイルを読み込む

---
+++ rust_read_manylines/src/main.rs
@@ -0,0 +1,43 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Write},
+};
+
+fn main() {
+    // 引数を取得
+    let args: Vec<String> = std::env::args().collect();
+    // 引数がなければ終了
+    if args.len() < 2 {
+        println!("Usage: {} [file]", args[0]);
+        return;
+    }
+
+    // pathを取得
+    let path = &args[1];
+
+    let mut count = 0;
+    // 時間計測開始
+    let start = std::time::Instant::now();
+
+    // ファイルを開く
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    // ループ
+    for line in reader.lines() {
+        line.unwrap();
+        count += 1;
+    }
+    // 時間計測終了
+    let end = start.elapsed();
+    println!("{} lines read in {:?}", count, end);
+}
+
+// 指定行数のファイルを作成
+fn write_file(line_count: usize) {
+    let path = "test.txt";
+    let mut file = File::create(path).unwrap();
+
+    let s: String = format!("{}\r", "rust,samplelogs,hello,world!").repeat(line_count);
+
+    file.write_all(s.as_bytes()).unwrap();
+}
